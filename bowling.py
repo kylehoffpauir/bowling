ff056308ee3f79911d8478068d202fc0fce042d6
Kyle <kyle@hoffpauir.com>
2023-10-14 18:02:58 -0400

    prep

--- bowling.py
+++ bowling.py
@@ -2,3 +2,23 @@ def Bowling():
     def __init__(self):
         return
 
+    # So what do we need to do?
+    # take in user input until we are done with the game
+    #   for each game, we do 10 frames
+    #   each frame is 2 throws
+    #   prompt user for score for throw 1
+    #   prompt user for score for throw 2
+    #   if the previous score was a strike, instead of simply adding to total score
+    #       we need to add 1.5x the next score, maybe have a flag set
+    #   on the 10th frame, if the user gets a strike/spare, continue throwing
+
+    # so thats just the method for running the game
+    # do we want to split this up into classes?
+
+
+
+
+
+def Game():
+    def __init__():
+        gameScore = 0
