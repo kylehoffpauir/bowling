398ed2b36725de51c34f0be8184ee0644058020b
Kyle <kyle@hoffpauir.com>
2023-10-16 11:34:52 -0400

    user add/load support. 10th frame logic working but a little messy. Adding a check so you cant roll over 10 pins in a single throw Overall, I'm pretty happy with it. it's not perfect but I think it's well thought out

--- bowling.py
+++ bowling.py
@@ -1,3 +1,6 @@
+# import to save/load user data dictionary
+import pickle
+
 class Game():
     """
     possible improvements:
@@ -5,22 +8,28 @@ class Game():
                                            would be simple enough - when a new game is started, pass in players array
                                            and loop through each player between frames but before throws. Give each
                                            player a gameScore dict
+
         clean up ui, maybe even add a gui. If we go really crazy we could make this a webapp and have it run in a browser
-        add a database to store scores and player names
-        add a way to view previous games' scoreboards
+
+        add a database to store scores and player names - right now just using python pickle library to save/load user info
+
+        add a way to view previous games' entire scoreboards -- right now we can only see the high scores of users
+
+        10th frame logic made things pretty messy. We could clean that up by only calling calculateScore() after the 10th
+            frame is complete. This would require a little bit of refactoring, but would reduce the number of if statements
+            in calculateScore() and would streamline the logic a bit.
+            This comes at the cost of having a scoreboard that doesn't update until the 10th frame is complete, which
+            is a little less user friendly. I think the current implementation is good, but it comes at the cost of
+            some messy if checks littered throughout the code.
     """
 
-    def __init__(self):
+    def __init__(self, user):
         self.gameScore = {1: [], 2: [], 3: [],
                      4: [], 5: [], 6: [],
                      7: [], 8: [], 9: [],
                      10: []}
-        self.name = self.getUser()
-
-    def getUser(self):
-        name = input("What is your name? ")
-        print("Hello " + name + "!")
-        return name
+        self.user = user
+        self.name = user.getName()
 
     def playGame(self):
         print("---- STARTING A NEW GAME ----")
@@ -37,7 +46,6 @@ class Game():
 
     def throwBall(self, frame, tenthFrame=False):
         # using this while loop instead of a for loop because we may need to repeat a throw on invalid input
-        # TODO add 10th frame logic
         throw = 1
         tenthThrow = 1
         while throw < 3:
@@ -45,6 +53,9 @@ class Game():
             pinsKnocked = input("Enter score: ").upper()
             # validate input
             if not self.validateScore(pinsKnocked, throw):
+                print("Invalid input, try again")
+                continue
+            if throw == 2 and self.gameScore[frame][0] + int(pinsKnocked) > 10:
                 print("Invalid input, try again")
                 continue
             # if we get a strike, we are done with the frame
@@ -70,11 +81,15 @@ class Game():
             # otherwise, add score to frame as normal
             else:
                 self.gameScore[frame].append(int(pinsKnocked))
+                if tenthFrame:
+                    tenthThrow += 1
+                    if tenthThrow > 4:
+                        break
                 throw += 1
 
     def validateScore(self, score, throw):
         # valid inputs are only [X, /, 1-10] IFF / is a / on the second throw
-        #print(str(score in ["X", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]) + " " + str(throw == 2 and score == "/"))
+        # print(str(score in ["X", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]) + " " + str(throw == 2 and score == "/"))
         return True if (throw == 2 and score == "/") \
             else (score in ["X", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
 
@@ -92,7 +107,7 @@ class Game():
             # if we are on the 10th frame, we need to handle the bonus throws, sum the frame and add to total
             if x == 10:
                 totalScore += sum(self.convert(element) for element in self.gameScore[x])
-                if self.gameScore[x-1][0] == "X" or self.gameScore[x-1][1] == "/":
+                if self.gameScore[ x -1][0] == "X" or self.gameScore[ x -1][1] == "/":
                     totalScore += sum(self.convert(element) for element in self.gameScore[x])
             # on a strike, add 10 + next two throws. if 9th frame, wait until 10th frame to add bonus throws
             elif self.gameScore[x][0] == "X":
@@ -102,7 +117,8 @@ class Game():
                     totalScore += 10 + (self.convert(self.gameScore[x + 1][0]) + self.convert(self.gameScore[x + 1][1]))
                 elif len(self.gameScore[x + 1]) == 1 and self.gameScore[x + 1][0] == "X":
                     if len(self.gameScore[x + 2]) > 0:
-                        totalScore += 10 + (self.convert(self.gameScore[x + 1][0]) + self.convert(self.gameScore[x + 2][0]))
+                        totalScore += 10 + \
+                                    (self.convert(self.gameScore[x + 1][0]) + self.convert(self.gameScore[x + 2][0]))
                 else:
                     totalScore += 10
             # on a spare, add 10 + next throw, if 9th frame, wait until 10th frame to add bonus throw
@@ -149,7 +165,6 @@ class Game():
     How to Score: The score for the 10th frame is the total number of pins knocked down in the 10th frame.
     """
 
-
     def printScore(self):
         # make output to look like a scoreboard for bowling
         print(self.name + "'s Scoreboard")
@@ -160,7 +175,7 @@ class Game():
         # print the results of each frame from the gameScore dictionary
         # updates in "real time" ie. after each throw - which is why we check the dict entry's array length
         for x in self.gameScore:
-            print("| ", end = "")
+            print("| ", end="")
             # if the frame is empty, print nothing for that frame
             if len(self.gameScore[x]) == 0:
                 print("\t\t", end="")
@@ -172,7 +187,8 @@ class Game():
                 print(" " + str(self.gameScore[x][0]) + "-" + str(self.gameScore[x][1]) + "\t", end="")
             # if the frame is more elements, print the score for each throw
             else:
-                print("" + str(self.gameScore[x][0]) + "-" + str(self.gameScore[x][1]) + "-" + str(self.gameScore[x][2]) + "", end="")
+                print("" + str(self.gameScore[x][0]) + "-" + str(self.gameScore[x][1]) + "-" + str(
+                    self.gameScore[x][2]) + "", end="")
 
         # the last thing to do is to print the right border (+ a newline)
         print("|")
@@ -180,17 +196,70 @@ class Game():
         print("| TOTAL = " + str(self.calculateScore()) + "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|")
         print("---------------------------------------------------------------------------------")
 
+class User():
+    def __init__(self, name=None):
+        self.name, self.highScore = self.getUser(name)
 
-# # start a new game on run
-# class User:
-#     __init__(self, name):
-#         self.name = name
-#
-#     __init__(self):
-#         self.name = "User"
+    def getName(self):
+        return self.name
+
+    def getHighScore(self):
+        return self.highScore
+
+    def getUser(self, name=None):
+        if name is not None:
+            name = input("What is your name? ")
+            print("Checking saved data for: " + name)
+        # check scorecard file for user entry
+        # if user exists, load their scorecard and highest score
+        # if user does not exist, create a new user entry
+        try:
+            with open('users.pkl', 'rb') as f:
+                loaded_dict = pickle.load(f)
+        except(FileNotFoundError):
+            loaded_dict = {}
+        if name in loaded_dict:
+            print("Welcome back " + name + "!")
+            print("High Score: " + str(loaded_dict[name]))
+            return name, loaded_dict[name]
+        else:
+            print("Welcome " + name + "!")
+            loaded_dict[name] = 0
+        try:
+            with open('users.pkl', 'wb') as f:
+                pickle.dump(loaded_dict, f)
+        except():
+            print("Error saving user data")
+        return name, 0
+
+    def saveScore(self, score):
+        # save the user's highest score
+        try:
+            with open('users.pkl', 'rb') as f:
+                loaded_dict = pickle.load(f)
+        except(FileNotFoundError):
+            loaded_dict = {}
+        if self.name in loaded_dict:
+            if score > loaded_dict[self.name]:
+                print("New High Score!")
+                print("Saving new high score for " + self.name)
+                loaded_dict[self.name] = score
+        else:
+            loaded_dict[self.name] = score
+        try:
+            with open('users.pkl', 'wb') as f:
+                pickle.dump(loaded_dict, f)
+        except():
+            print("Error saving user data")
 
 
-# kyle = User("Kyle")
-game = Game()
+# start the game on run
+# create a user
+kyle = User("Kyle")
+# create a game
+game = Game(kyle)
+# play the game
 score = game.playGame()
 print("Final Score: " + str(score))
+# save the user's score
+kyle.saveScore(score)
